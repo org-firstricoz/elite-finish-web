dab19d57c63edf53d094a371c9528d99cc4d8e3f
Deepak <deepak2603dk@gmail.com>
2024-10-05 14:36:54 +0530

    feat: service hero responsive

--- src/pages/customer/services/components/Services.tsx
+++ src/pages/customer/services/components/Services.tsx
@@ -10,13 +10,13 @@ const Services:React.FC = () => {
     const heroRef = React.useRef<HTMLDivElement>(null)
   return (
     <EliteWideScreenHandler>
-        <div className="relative h-auto bg-[#F9F8FF]">
+        <div className="relative h-auto bg-[#F9F8FF] overflow-hidden">
         <div ref={heroRef} className="absolute z-40 -top-48 md:-top-24   h-[11rem] w-full"/>
 
             <Navbar heroRef={heroRef}/>
         <div className=" flex justify-end">
-            <img src={Brush} alt="brush" />
-            <EliteHeading classname="absolute left-[50%]  top-[65%] translate-x-[-50%] translate-y-[-50%]">
+            <img src={Brush} alt="brush" className="md:translate-x-28 translate-x-48" />
+            <EliteHeading classname="absolute left-[50%]  sm:w-auto w-10/12  font-bold  top-[70%] translate-x-[-50%] translate-y-[-50%]">
             We’re highly rated painting service provider.
             </EliteHeading>
         </div>
