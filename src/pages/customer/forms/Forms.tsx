2efe207da3a6a34c8cb9e0eaff7c2fade909176b
Madhuresh Verma <xlongclaw@gmail.com>
2025-02-03 13:31:02 +0530

    fix form

--- src/pages/customer/forms/Forms.tsx
+++ src/pages/customer/forms/Forms.tsx
@@ -1,13 +1,13 @@
 import { useParams } from "react-router-dom";
 
 const Forms = () => {
-  const { form_title } = useParams();
+  const { form_title } = useParams<{ form_title: string }>();
+
   return (
-    <div className=" overflow-hidden w-screen h-screen">
-      {/* {form_title} */}
+    <div className="overflow-hidden w-screen h-screen">
       <iframe
-        className=" w-full h-full overflow-hidden"
-        src={`https:/form.ricoz.in/projects/679de55a02ed7a627648db04/form/by-title/${form_title}`}
+        className="w-full h-full overflow-hidden"
+        src={`https://form.ricoz.in/projects/679de55a02ed7a627648db04/form/by-title/${form_title}`}
       ></iframe>
     </div>
   );
